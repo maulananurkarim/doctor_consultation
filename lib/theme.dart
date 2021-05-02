bf21e4fb85225146cdd04a291a71ae713bf07c26
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-02 23:59:14 +0700

    commit

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,3 +2,15 @@ import 'package:flutter/material.dart';
 
 Color blueColor = Color(0xff252E4E);
 Color greyColor = Color(0xffC3C8D8);
+
+TextStyle titleTextStyle = TextStyle(
+  color: blueColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 20,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greyColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 12,
+);
