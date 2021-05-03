c2d199e34320fae02399178313b5051ee69b1171
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-03 10:52:36 +0700

    commit

--- lib/rating.dart
+++ lib/rating.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_consultation/main.dart';
 import 'package:doctor_consultation/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +20,67 @@ class _RatingState extends State<Rating> {
               colors: [lightBlueColor, Colors.white],
             ),
           ),
-          child: Column(
-            children: [],
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                Text(
+                  "Hope you are",
+                ),
+                Text(
+                  "feeling better now!",
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Image.asset(
+                  "assets/user_pic1.png",
+                  height: 150,
+                  width: 150,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Alessya Camella",
+                ),
+                Text(
+                  "Eye Specialist",
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset(
+                  "assets/rating.png",
+                  height: 40,
+                  width: 232,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => Home(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Rate Alessya",
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "Report for Bad Service",
+                ),
+              ],
+            ),
           ),
         ),
       ),
